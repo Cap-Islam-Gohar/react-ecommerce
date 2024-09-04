8af4942dfd892e28bf72ff00448eefd3c2671745
Cap-Islam-Gohar <cap.islamadel@gmail.com>
2024-09-04 18:38:00 +0300

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ export default function App() {
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/card" element={<Guard><Cart /></Guard>} />
-                    <Route path="/product" element={<Guard><Product /></Guard>} />
+                    <Route path="/product:id" element={<Guard><Product /></Guard>} />
                     <Route path="/brands" element={<Guard><Brands /></Guard>} />
                     <Route path="/categories" element={<Guard><Categories /></Guard>} />
                     <Route path="/wishlist" element={<Guard><Wishlist /></Guard>} />
