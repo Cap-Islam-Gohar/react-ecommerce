35b13615d161be959125d7de23eef114e02462b5
Cap-Islam-Gohar <cap.islamadel@gmail.com>
2024-09-04 18:18:53 +0300

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,43 +12,25 @@ import Brands from './Components/Pages/Brands'
 import { Provider } from 'react-redux';
 import Store from './Redux/Store';
 import Wishlist from './Components/Pages/Wishlist';
-import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 export default function App() {
 
-	const router = createBrowserRouter(
-        [{ 
-            path: '', 
-            element: <Layout />, 
-            errorElement: <NotFound/>,
-            children: [
-                { path : '', index: true, element: <Guard><Home /></Guard>  },
-                { path: 'register', element: <Register /> },
-                { path: 'login', element: <Login /> },
-                { path: 'product/:id', element: <Guard><Product/></Guard> },
-                { path: 'cart', element: <Guard><Cart/></Guard> },
-                { path: 'wishlist', element: <Guard><Wishlist/></Guard> },
-                { path: 'categories', element: <Guard><Categories/></Guard> },
-                { path: 'brands', element: <Guard><Brands/></Guard> }
-            ]
-        }] ,{
-            basename: "/"
-        });
-
-
 	return <Provider store={Store}>
-        {/* <BrowserRouter>
+        <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Guard><Home /></Guard>} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/card" element={<Guard><Card /></Guard>} />
-                <Route path="/brands" element={<Guard><Brands /></Guard>} />
-                <Route path="/categories" element={<Guard><Categories /></Guard>} />
-                <Route path="/wishlist" element={<Guard><Wishlist /></Guard>} />
+                <Route path="/" element={<Layout />}>
+                    <Route path="/" element={<Guard><Home /></Guard>} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/card" element={<Guard><Cart /></Guard>} />
+                    <Route path="/product" element={<Guard><Product /></Guard>} />
+                    <Route path="/brands" element={<Guard><Brands /></Guard>} />
+                    <Route path="/categories" element={<Guard><Categories /></Guard>} />
+                    <Route path="/wishlist" element={<Guard><Wishlist /></Guard>} />
+                </Route>
             </Routes>
-        </BrowserRouter> */}
-        <RouterProvider router={router} />
+        </BrowserRouter>
 	</Provider>
 }
