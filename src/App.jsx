44421c3f9b8ed1373e205413356215a01997be18
Cap-Islam-Gohar <cap.islamadel@gmail.com>
2024-09-04 18:01:56 +0300

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,38 +12,38 @@ import Brands from './Components/Pages/Brands'
 import { Provider } from 'react-redux';
 import Store from './Redux/Store';
 import Wishlist from './Components/Pages/Wishlist';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter } from 'react-router-dom';
 
 
 export default function App() {
 
-	// const router = createBrowserRouter([
-	// 	{ 
-	// 		path: '/', 
-	// 		element: <Layout />, 
-	// 		errorElement: <NotFound/>,
-	// 		children: [
-	// 			{ path : '', index: true, element: <Guard><Home /></Guard>  },
-	// 			{ path: 'register', element: <Register /> },
-	// 			{ path: 'login', element: <Login /> },
-	// 			{ path: 'product/:id', element: <Guard><Product/></Guard> },
-	// 			{ path: 'cart', element: <Guard><Cart/></Guard> },
-    //             { path: 'wishlist', element: <Guard><Wishlist/></Guard> },
-    //             { path: 'categories', element: <Guard><Categories/></Guard> },
-    //             { path: 'brands', element: <Guard><Brands/></Guard> }
-	// 		]
-	// 	},
-	// ]);
+	const router = createBrowserRouter([
+		{ 
+			path: '/', 
+			element: <Layout />, 
+			errorElement: <NotFound/>,
+			children: [
+				{ path : '', index: true, element: <Guard><Home /></Guard>  },
+				{ path: 'register', element: <Register /> },
+				{ path: 'login', element: <Login /> },
+				{ path: 'product/:id', element: <Guard><Product/></Guard> },
+				{ path: 'cart', element: <Guard><Cart/></Guard> },
+                { path: 'wishlist', element: <Guard><Wishlist/></Guard> },
+                { path: 'categories', element: <Guard><Categories/></Guard> },
+                { path: 'brands', element: <Guard><Brands/></Guard> }
+			]
+		},
+	]);
 
 
 	return <Provider store={Store}>
-        <BrowserRouter>
+        {/* <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
             </Routes>
-        </BrowserRouter>
-        {/* <RouterProvider router={router} /> */}
+        </BrowserRouter> */}
+        <RouterProvider router={router} />
 	</Provider>
 }
